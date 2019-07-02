8d5da0f1e489cc5bfa49b28f874f52336a5b2460
fame2105 <fame2105@gmail.com>
2019-07-03 00:07:15 +0530

    Merged changes from master_b to master

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -36,7 +36,7 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
         int count = petTypeService.findAll().size();
         if(count > 0){
-            loadData();;    
+            loadData();
         }
         
     }
